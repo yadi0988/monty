8a15bfec5e7323f7e9b5ef27fdbe63b3a644dbca
yadi09 <terefeyadamzer@gmail.com>
2024-01-29 23:16:29 +0100

    All in one

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of argument
+ * @argv: arguments
+ * Return: int
+ **/
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+    {
+      fprintf(stderr, "USAGE: monty file\n");
+      exit(EXIT_FAILURE);
+    }
+
+  processor(argv[1], 0);
+
+  return (0);
+}
