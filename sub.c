46d8ddff1b5833b7431c14f7cb4fd225bb0c4ee4
yadi09 <terefeyadamzer@gmail.com>
2024-01-30 12:52:29 +0100

    ..

--- sub.c
+++ sub.c
@@ -12,7 +12,7 @@ void sub(stack_t **stack, unsigned int line_number)
   stack_t *temp;
   if (!stack || *stack == NULL || (*stack)->prev == NULL)
     {
-      fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+      fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
       exit(EXIT_FAILURE);
     }
 
