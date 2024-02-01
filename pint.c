df9f439313c7d2b94bc75e1778fde94175cc63d5
yadi09 <terefeyadamzer@gmail.com>
2024-02-01 11:48:09 +0100

    ..

--- pint.c
+++ pint.c
@@ -9,7 +9,7 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-  if ((*stack) == NULL || !stack)
+  if (*stack == NULL)
     {
       fprintf(stderr, "L%d: can't pint, stack empty", line_number);
       exit(EXIT_FAILURE);
